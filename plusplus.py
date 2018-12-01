97a806e12f9c7f68479e6189a74a4f538dad50a4
Kyosti Alkio <kyosti.alkio@gmail.com>
2018-12-01 20:31:50 +0200

    adds better unicode support

--- plusplus.py
+++ plusplus.py
@@ -1,17 +1,11 @@
 # Author falarikae
 
-from string import ascii_lowercase as asl
-
-asl = ''.join((' ', asl, 'åäö'))
-
-
 def fromPlus(str):
-    arr = [asl[len(i) - 1] for i in str.split()]
+    arr = [chr(len(i)) for i in str.split()]
     out = ''.join(arr)
     return out
 
-
 def toPlus(str):
-    arr = ['+' * (asl.find(i.lower()) + 1) for i in str]
+    arr = ['+' * ord(i) for i in str]
     out = ' '.join(arr)
     return out
